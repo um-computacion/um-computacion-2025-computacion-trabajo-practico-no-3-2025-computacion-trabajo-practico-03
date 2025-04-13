0a3c9dfac9aa0eb05b6a97f2d2cd0beecc49ecb6
Ezequiel-B18 <eablajevitch@gmail.com>
2025-04-13 01:18:27 -0300

    Tests para texto no numerico

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -17,6 +17,10 @@ class TestCalculoNumeros(unittest.TestCase):
     def test_ingreso_negativo(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
             ingrese_numero()
-
+    @patch('builtins.input', return_value='AAA')
+    def test_ingreso_letras(self, patch_input):
+        with self.assertRaises(ValueError):
+            ingrese_numero()
+            
 if __name__ == '__main__':
     unittest.main()
