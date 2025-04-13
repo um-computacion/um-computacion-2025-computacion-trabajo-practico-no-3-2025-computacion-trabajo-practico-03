8de1bf2ffcb0bd560908909ff29e52091a378356
Ezequiel-B18 <eablajevitch@gmail.com>
2025-04-13 17:00:31 -0300

    Implementacion de manejo de excepciones

--- src/calculo_numeros.py
+++ src/calculo_numeros.py
@@ -2,7 +2,7 @@ from src.exceptions import NumeroDebeSerPositivo
 
 def ingrese_numero():
     try:
-        num = int(input("Ingrese un numero:"))
+        num = int(input("Ingrese un numero: "))
 
         if num < 0:
             raise NumeroDebeSerPositivo()
@@ -11,3 +11,19 @@ def ingrese_numero():
     except ValueError:
         raise ValueError("La entrada debe ser un numero valido.")
 
+def main():
+    while True:
+        try:
+            num = ingrese_numero()
+            print(f"Número valido: {num}")
+        except NumeroDebeSerPositivo as error1:
+            print(f"Error: {error1}")
+        except ValueError as error2:
+            print(f"Error: {error2}")
+
+        continuar = input("Desea seguir? (s/n): ")
+        if continuar.lower() != "s":
+            break
+
+if __name__ == "__main__":
+    main()
--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -11,10 +11,12 @@ class TestCalculoNumeros(unittest.TestCase):
     def test_ingreso_feliz(self, patch_input):
         numero = ingrese_numero()
         self.assertEqual(numero, 100)
+
     @patch('builtins.input', return_value='-100')
     def test_ingreso_negativo(self, patch_input):
         with self.assertRaises(NumeroDebeSerPositivo):
             ingrese_numero()
+    
     @patch('builtins.input', return_value='AAA')
     def test_ingreso_letras(self, patch_input):
         with self.assertRaises(ValueError):
